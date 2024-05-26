9da2c6e8cf21ad54d6351f90b664a87ab769794a
Jeramie Santos <jeramieramsantos79@gmail.com>
2024-05-26 11:10:57 +0800

    cleaning js

--- main.js
+++ main.js
@@ -5,13 +5,13 @@ const myLibrary = [
         title: "The Hobbit",
         author: "J.R.R Tolkien",
         pages: 295,
-        isRead: "Yes"
+        isRead: "No"
     }, 
     {
         title: "Subtle art of not giving a f*ck",
         author: "Mark Manson",
         pages: 206,
-        isRead: "No"
+        isRead: "Yes"
     },
     {
         title: "Atomic Habits",
@@ -28,93 +28,60 @@ function Book(title, author, pages, isRead) {
     this.isRead = isRead;
 }
 
+const btnOpen = document.querySelector("header button");
+const btnClose = document.querySelector(".btn-close");
+const dialog = document.querySelector("dialog");
+const btnSubmit = document.querySelector(".btn-submit");
 
 const main = document.querySelector("main");
 const container = document.createElement("div");
+
 container.setAttribute("class", "container");
 main.appendChild(container);
 
 function displayBook(){
-    
-    if (initialize) {
-        for (let i = 0; i < myLibrary.length; i++){
-            let card = document.createElement("div");
-            let titleContainer = document.createElement("div");
-            let infoContainer = document.createElement("div");
 
-            let title = document.createElement("h2");
-            let author = document.createElement("p");
-            let pages = document.createElement("p");
-            let read = document.createElement("p");
-        
-            card.className = "card";
-            author.className = "author";
-            titleContainer.className = "title-card";
-            infoContainer.className = "info-card";
-
-
-           
-
-            title.textContent = myLibrary[i].title;
-            author.textContent = `by ${myLibrary[i].author}`;
-            pages.textContent = `Total pages: ${myLibrary[i].pages}`;
-            read.textContent = `Read Status:  ${myLibrary[i].isRead}`;
-
-            titleContainer.appendChild(title);
-        
-            
-            infoContainer.appendChild(author);
-            infoContainer.appendChild(pages);
-            infoContainer.appendChild(read);
-
-            card.appendChild(titleContainer);
-            card.appendChild(infoContainer);
-        
-            container.appendChild(card);
+    if (!initialize) {
+        const total = document.querySelectorAll(".card");
+        for (let i = 0; i < total.length; i++){
+            container.removeChild(total[i]);
         }
-    } else {
-            const total = document.querySelectorAll(".card");
-            for (let i = 0; i < total.length; i++){
-                container.removeChild(total[i]);
-            }
+    }
 
-            for (let i = 0; i < myLibrary.length; i++){
-                let card = document.createElement("div");
-                let titleContainer = document.createElement("div");
-                let infoContainer = document.createElement("div");
+    for (let i = 0; i < myLibrary.length; i++){
+        let card = document.createElement("div");
+        let titleContainer = document.createElement("div");
+        let infoContainer = document.createElement("div");
 
-                let title = document.createElement("h2");
-                let author = document.createElement("p");
-                let pages = document.createElement("p");
-                let read = document.createElement("p");
-            
-                card.className = "card";
-                author.className = "author";
-                titleContainer.className = "title-card";
-                infoContainer.className = "info-card";
+        let title = document.createElement("h2");
+        let author = document.createElement("p");
+        let pages = document.createElement("p");
+        let read = document.createElement("p");
+    
+        card.className = "card";
+        author.className = "author";
+        titleContainer.className = "title-card";
+        infoContainer.className = "info-card";
 
-                title.textContent = myLibrary[i].title;
-                author.textContent = `by ${myLibrary[i].author}`;
-                pages.textContent = `Total pages: ${myLibrary[i].pages}`;
-                read.textContent = `Read Status: ${myLibrary[i].isRead}`;
+        title.textContent = myLibrary[i].title;
+        author.textContent = `by ${myLibrary[i].author}`;
+        pages.textContent = `Total pages: ${myLibrary[i].pages}`;
+        read.textContent = `Read Status:  ${myLibrary[i].isRead}`;
 
-                titleContainer.appendChild(title);
-            
-                
-                infoContainer.appendChild(author);
-                infoContainer.appendChild(pages);
-                infoContainer.appendChild(read);
+        titleContainer.appendChild(title);
+    
+        infoContainer.appendChild(author);
+        infoContainer.appendChild(pages);
+        infoContainer.appendChild(read);
 
-                card.appendChild(titleContainer);
-                card.appendChild(infoContainer);
-            
-                container.appendChild(card);
-                main.appendChild(container);
-            }
-            
+        card.appendChild(titleContainer);
+        card.appendChild(infoContainer);
+    
+        container.appendChild(card);
     }
     
 }
+
 
 displayBook();
 
@@ -135,8 +102,6 @@ function addBookToLibrary(event) {
     const bookPage = document.querySelector("#book-page").value;
     const bookRead = document.querySelector("#book-read");
     let isRead;
-    
-    // console.log(`${bookTitle} by ${bookAuthor}, ${bookPage} pages, ${bookRead}`);
 
     if (bookRead.checked == true) {
         isRead = "Yes";
@@ -157,19 +122,11 @@ function addBookToLibrary(event) {
 }
 
 
-const btnOpen = document.querySelector("header button");
-const btnClose = document.querySelector(".btn-close");
-const dialog = document.querySelector("dialog");
-const btnSubmit = document.querySelector(".btn-submit");
-let close = false;
-
-
 btnOpen.addEventListener("click", () => {
     dialog.showModal();
 });
 
 btnClose.addEventListener("click", () => {
-    close = true;
     dialog.close();
     addForm.reset();
 });
